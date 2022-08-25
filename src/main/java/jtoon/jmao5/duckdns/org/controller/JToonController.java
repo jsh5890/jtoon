1d755fa1e36afa42c02caf34d9a55e04fb3d8936
jsh5890 <jsh02352@naver.com>
2022-08-25 17:05:19 +0900

    test

--- src/main/java/jtoon/jmao5/duckdns/org/controller/JToonController.java
+++ src/main/java/jtoon/jmao5/duckdns/org/controller/JToonController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JToonController {
 
-    @GetMapping("/")
-    public String hello(){
-        return "ㅎㅇ";
-    }
+//    @GetMapping("/")
+//    public String hello(){
+//        return "ㅎㅇ";
+//    }
 }
